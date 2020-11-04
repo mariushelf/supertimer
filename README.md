c0961e036b9b55c408964b53e5e6d6ecf769fe56
Marius Helf <marius@xomnia.com>
2020-11-04 15:15:32 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# supertimer
+
+* Author: Marius Helf 
+  ([helfsmarius@gmail.com](mailto:helfsmarius@gmail.com))
+
+Contextmanager to print or log execution time of code blocks
+
+# License
+
+[MIT](https://choosealicense.com/licenses/mit)
+
+
---
+++ supertimer/timer.py
@@ -0,0 +1,55 @@
+import datetime as dt
+import logging
+
+module_logger = logging.getLogger(__name__)
+
+
+class timer:
+    """ Context manager that prints and/or logs the duration of the code block.
+
+    Parameters
+    ----------
+    description : str, optional
+        description of the timer. Used in log messages. Default: "Timer"
+    print: bool, optional
+        whether to print the log messages. Default: False
+    loglevel: int, optional
+        loglevel of logmessages produced by the timer. If ``None``, nothing is logged
+        (useful if `print` is True). default: ``logging.DEBUG``.
+    logger : logging.Logger, optional
+        which logger to use. Default: a logger with the name of this module
+
+    Examples
+    --------
+    with timer("sleep a while", loglevel=logger.INFO):
+        time.sleep(2)
+    """
+
+    def __init__(
+        self,
+        description: str = None,
+        print: bool = False,
+        loglevel: int = logging.DEBUG,
+        logger: logging.Logger = None,
+    ):
+        self.decription = description or "Timer"
+        self.print = print
+        self.loglevel = loglevel
+        self.logger = logger or module_logger
+
+    def __enter__(self):
+        self.start = dt.datetime.now()
+        msg = f"{self.description} starting at {self.start}"
+        if self.print:
+            print(msg)
+        if self.loglevel is not None:
+            self.logger.log(self.loglevel, msg)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end = dt.datetime.now()
+        duration = self.end - self.start
+        msg = f"{self.decription} finished at {self.end} after {duration}."
+        if self.print:
+            print(msg)
+        if self.loglevel is not None:
+            self.logger.log(self.loglevel, msg)
